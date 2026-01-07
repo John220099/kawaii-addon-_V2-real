e6f37dd0d224f2a9ea6ad5e410fbd7118471f3d0
john220099 <148237962+John220099@users.noreply.github.com>
2026-01-07 18:32:16 +0100

    fix render

--- src/main/java/kawaii/addon/v2/real/hud/HudCat.java
+++ src/main/java/kawaii/addon/v2/real/hud/HudCat.java
@@ -8,10 +8,8 @@ import meteordevelopment.meteorclient.utils.render.color.Color;
 import net.minecraft.util.Identifier;
 
 public class HudCat extends HudElement {
-    //TODO make it render
     public static final HudElementInfo<HudCat> INFO = new HudElementInfo<>(AddonTemplate.HUD_GROUP, "cat-hud", "Displays a cat icon.", HudCat::new);
-    public static final Identifier TEXTURE = Identifier.of("kawaii-addon", "textures/hud/cat1.png");
-    private final Identifier catgirl = Identifier.of("blackout", "catgirl.png");
+    public static final Identifier TEXTURE = Identifier.of("kawaii-addon", "/hud/cat1.png");
     public HudCat() {
         super(INFO);
     }
