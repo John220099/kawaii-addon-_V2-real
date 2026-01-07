3d28505d92a5ee194b8d40c696177b2989004ccc
john220099 <148237962+John220099@users.noreply.github.com>
2026-01-07 18:51:38 +0100

    size slider

--- src/main/java/kawaii/addon/v2/real/hud/HudCat.java
+++ src/main/java/kawaii/addon/v2/real/hud/HudCat.java
@@ -1,6 +1,9 @@
 package kawaii.addon.v2.real.hud;
 
 import kawaii.addon.v2.real.AddonTemplate;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.hud.HudElement;
 import meteordevelopment.meteorclient.systems.hud.HudElementInfo;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
@@ -14,9 +17,22 @@ public class HudCat extends HudElement {
         super(INFO);
     }
 
+    private final SettingGroup sg = settings.getDefaultGroup();
+
+    private final Setting<Integer> size = sg.add(new IntSetting.Builder()
+        .name("size")
+        .description("How many cats to show.")
+        .defaultValue(5)
+        .min(1)
+        .sliderMin(1)
+        .sliderMax(20)
+        .build()
+    );
+
     @Override
     public void render(HudRenderer renderer) {
-        setSize(64, 64);
+        int n = size.get();
+        setSize(64 * n, 64 * n);
         renderer.texture(TEXTURE, x, y, getWidth(), getHeight(), Color.WHITE);
     }
 }
