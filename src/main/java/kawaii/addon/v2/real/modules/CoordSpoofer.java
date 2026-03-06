16fa3ee85b268e932b6c0157008ccafe2774e46b
john220099 <148237962+John220099@users.noreply.github.com>
2026-03-06 20:29:20 +0100

    epic slider fix

--- src/main/java/kawaii/addon/v2/real/modules/CoordSpoofer.java
+++ src/main/java/kawaii/addon/v2/real/modules/CoordSpoofer.java
@@ -2,7 +2,6 @@ package kawaii.addon.v2.real.modules;
 
 import kawaii.addon.v2.real.KawaiiAddon;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.settings.*;
 
 import net.minecraft.util.math.random.Random;
@@ -15,8 +14,8 @@ public class CoordSpoofer extends Module {
         .name("seed")
         .description("Seed used to offset coordinates.")
         .defaultValue(Random.create().nextBetween(-1000000, 1000000))
-        .min(-1000000)
-        .max(1000000)
+        .min(0)
+        .sliderMax(1000000)
         .build()
     );
 
